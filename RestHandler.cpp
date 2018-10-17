c71899db9328e5384fc6faab6f3bd98090ab91f0
Ståle Kopperud <bsek@ovitas.no>
2018-10-17 16:24:03 +0200

    Added UTF-8 converter

--- RestHandler.cpp
+++ RestHandler.cpp
@@ -1,9 +1,38 @@
 #include <future>
 #include <iostream>
+#include <iconv.h>
+#include <type_traits>
 #include "RestHandler.h"
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
+
+char * convertUtf8toAscii(std::string &input) {
+    size_t inbytes = input.length();
+    char *dest_str = new char[inbytes];
+    char *input_str = new char[inbytes];
+    strcpy(input_str, input.c_str());
+
+    char *dest = dest_str;
+
+    size_t outbytes = sizeof dest_str;
+    iconv_t conv = iconv_open("ISO8859-1", "UTF-8");
+
+    if (conv == (iconv_t)-1) {
+        perror("iconv_open");
+        return nullptr;
+    }
+
+    if (iconv(conv, &input_str, &inbytes, &dest, &outbytes) == (size_t)-1) {
+        perror("iconv");
+        return nullptr;
+    }
+
+    dest_str[sizeof dest_str - outbytes] = 0;
+    puts(dest_str);
+
+    return dest_str;
+}
 
 void RestHandler::setup() {
     mux.handle("/start").post([&](served::response &res, const served::request &req) {
@@ -30,13 +59,17 @@ void RestHandler::setup() {
             
             if (parsedMessage != nullptr) {
                 try {
-                    auto str = parsedMessage.at("message").get<std::string>();
+                    std::string str = parsedMessage.at("message").get<std::string>();
                     auto times = 3;
 
                     if (parsedMessage.find("times") != parsedMessage.end()) {
                         times = parsedMessage.at("times").get<std::int32_t>();
                     }
-                    scroller.setupText(times, str);
+                    char * text = convertUtf8toAscii(str);
+                    //char * text = new char[str.length()];
+                    //strcpy(text, str.c_str());
+                    std::string inputStr(text);
+                    scroller.setupText(times, inputStr);
                 } catch (json::out_of_range& e) {
                     std::cout << "message: " << e.what() << '\n'
                               << "exception id: " << e.id << std::endl;
